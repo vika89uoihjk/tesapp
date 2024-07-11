186f1e6fd2fbc2c5b822417cfd15280888374b09
vika89uoihjk <vikramathithan7686@gmail.com>
2024-07-11 17:17:00 +0530

    Update test.java

--- domain/src/test.java
+++ domain/src/test.java
@@ -3,7 +3,7 @@ public class test extends Application {
     public void onCreate() {
         super.onCreate();
         // Creating an extended library configuration.
-        AppMetricaConfig config = AppMetricaConfig.newConfigBuilder(4c5bd7fb-525b-42c4-9dfe-031105271680).build();
+        AppMetricaConfig config = AppMetricaConfig.newConfigBuilder(6aede49e-85f6-4a86-bd17-94346a089ad2).build();
         // Initializing the AppMetrica SDK.
         AppMetrica.activate(this, config);
     }
